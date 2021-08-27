524987de42cc676a627b030528b15c1990d87782
Strohm Armstrong <strohma@amazon.com>
2021-08-27 08:36:43 -0700

    Convert to SASS

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 export default function About() {
   console.log("in About");
